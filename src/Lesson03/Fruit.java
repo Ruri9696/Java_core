a10c9990b12c6eece9d369d14f45bcc9ef0279fe
ruri <ruri@mail.ru>
2021-10-18 13:41:23 +0300

    Initial commit

---
+++ src/Lesson03/Fruit.java
@@ -0,0 +1,13 @@
+package Lesson03;
+
+abstract class Fruit {
+    private float weight;
+
+    Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
